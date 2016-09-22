5cf7dafdaacf7a13e1004e47a047595ad0fe60be
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2016-09-22 16:37:41 +0200

    Add button to view list from main

--- app/src/main/java/se/chalmers/cid/MainActivity.java
+++ app/src/main/java/se/chalmers/cid/MainActivity.java
@@ -1,7 +1,9 @@
 package se.chalmers.cid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 	}
 
+	/** Called when the user clicks the button
+	 * Only implemented to demonstrate the mentorlist view
+	 *
+	 * */
+	public void changeViewToMentors(View view) {
+		Intent intent = new Intent(this, MentorListActivity.class);
+		startActivity(intent);
+	}
 
 }
---
+++ app/src/main/java/se/chalmers/cid/MentorListActivity.java
@@ -0,0 +1,19 @@
+package se.chalmers.cid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MentorListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_list);
+
+       // Intent intent = getIntent();
+
+
+
+    }
+}
