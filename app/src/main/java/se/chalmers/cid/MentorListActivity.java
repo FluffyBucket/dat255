31bf82963425212315a5d7b9b5fdd437a33a08f7
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2016-09-24 21:57:13 +0200

    Add random names to list, change back to List_View from Expandable_List_View for simplicity

--- app/src/main/java/se/chalmers/cid/MentorListActivity.java
+++ app/src/main/java/se/chalmers/cid/MentorListActivity.java
@@ -3,16 +3,24 @@ package se.chalmers.cid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MentorListActivity extends AppCompatActivity {
+
+    String[] randomData = {"Fredrik", "Per-Einar", "Fritjolf", "Rudolf", "Emilie", "Håkon", "Snoop Dogg"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_list);
-        //this comment tho
 
-       // Intent intent = getIntent();
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listitem, randomData);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
 
 
 
